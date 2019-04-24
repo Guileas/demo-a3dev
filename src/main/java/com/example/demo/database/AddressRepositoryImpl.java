43ab95559bc4af4b3740c7d8f0ae2257472c4d4e
noobzor <fegbarthas@gmail.com>
2019-04-24 16:50:49 +0200

    impl adressrepository + userrepository

---
+++ src/main/java/com/example/demo/database/AddressRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.example.demo.database;
+
+import com.example.demo.entities.Address;
+
+public class AddressRepositoryImpl extends JpaRepositoryImpl<Address> implements AddressRepository {
+
+}
---
+++ src/main/java/com/example/demo/database/UserRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.example.demo.database;
+
+import com.example.demo.entities.User;
+
+public class UserRepositoryImpl extends JpaRepositoryImpl<User> implements UserRepository{
+
+}
