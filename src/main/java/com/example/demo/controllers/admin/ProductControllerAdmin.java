efaa6e4c92a18e327470ded9bb205a0d9888495b
noobzor <fegbarthas@gmail.com>
2019-04-25 12:16:00 +0200

    role + product

---
+++ src/main/java/com/example/demo/controllers/admin/ProductControllerAdmin.java
@@ -0,0 +1,20 @@
+package com.example.demo.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.controllers.admin.base.BaseAdminController;
+import com.example.demo.controllers.admin.base.BaseAdminLinkedController;
+import com.example.demo.controllers.utils.UriUtils;
+import com.example.demo.entities.Product;
+
+@Controller
+@RequestMapping(value = {UriUtils.URI_SLASH + BaseAdminController.BASE_ADMIN_CONTROLLER_NAME + UriUtils.URI_SLASH + ProductControllerAdmin.PRODUCT_CONTROLLER_ADMIN_NAME})
+public class ProductControllerAdmin extends BaseAdminLinkedController<Product> {
+
+	public static final String PRODUCT_CONTROLLER_ADMIN_NAME = "product";
+
+    protected ProductControllerAdmin() {
+        super(PRODUCT_CONTROLLER_ADMIN_NAME, Product.class);
+    }
+}
